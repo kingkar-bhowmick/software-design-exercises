0afeb009e57ea35d899a4287bb1fef1d42e08d3a
Daniel Retkowitz <daniel.retkowitz@hs-niederrhein.de>
2025-10-28 13:58:44 +0100

    Added code for exercise 04

---
+++ ex04/observer/src/ConcreteObservable.java
@@ -0,0 +1,21 @@
+public class ConcreteObservable implements Observable {
+    // TODO: private List<Observer> observers;
+    // TODO: Beispielzustand, z. B. private double temperature;
+
+    @Override
+    public void addObserver(Observer o) {
+        /* TODO */
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        /* TODO */
+    }
+
+    @Override
+    public void notifyObservers() {
+        /* TODO */
+    }
+
+    // TODO: Getter/Setter für den Zustand (z. B. getTemperature/setTemperature)
+}
