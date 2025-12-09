8bb2ce4e6923e91a79fc13b2275cd55e25374085
Daniel Retkowitz <daniel.retkowitz@hs-niederrhein.de>
2025-12-09 14:11:21 +0100

    Added code for exercise 08

---
+++ ex08/discount-rule-service/src/main/java/com/shop/discounts/DiscountRuleRepository.java
@@ -0,0 +1,6 @@
+package com.shop.discounts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiscountRuleRepository extends JpaRepository<DiscountRule, Integer> {
+}
---
+++ ex08/discount-rule-service/src/main/java/com/shop/discounts/DiscountTarget.java
@@ -0,0 +1,6 @@
+package com.shop.discounts;
+
+public enum DiscountTarget {
+    ORDER,          // Rabatt auf gesamte Bestellung
+    CUSTOMER_GROUP  // Rabatt auf bestimmte Kundengruppe
+}
